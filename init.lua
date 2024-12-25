7c51b6ddbeda449bbd545efde348239585baa00c
Luis V <luisvinicius0906@gmail.com>
2024-12-25 19:06:14 -0300

    add autopairs plugin require

--- init.lua
+++ init.lua
@@ -288,7 +288,7 @@ require('lazy').setup({
 
   -- require 'kickstart.plugins.lint',
 
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
 
   require 'kickstart.plugins.neo-tree',
 

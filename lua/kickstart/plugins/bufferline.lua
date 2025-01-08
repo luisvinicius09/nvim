9f23b78be96d7e217e165bfe8f7c2cdfa8d28fd5
Luis Vinicius <luisvinicius0906@gmail.com>
2025-01-08 11:07:34 -0300

    fix bufferline keymaps switched

--- lua/kickstart/plugins/bufferline.lua
+++ lua/kickstart/plugins/bufferline.lua
@@ -4,8 +4,8 @@ return {
     version = '*',
     dependencies = 'nvim-tree/nvim-web-devicons',
     keys = {
-      { '<S-H>', '<Cmd>BufferLineCycleNext<CR>', desc = 'Next tab' },
-      { '<S-L>', '<Cmd>BufferLineCyclePrev<CR>', desc = 'Prev tab' },
+      { '<S-L>', '<Cmd>BufferLineCycleNext<CR>', desc = 'Next tab' },
+      { '<S-H>', '<Cmd>BufferLineCyclePrev<CR>', desc = 'Prev tab' },
       { '<leader>x', ':bdelete<CR>', desc = 'Close tab', { noremap = true, silent = true } },
       { '<leader>bp', '<Cmd>BufferLinePickClose<CR>', desc = 'Close tab picked' },
       { '<leader>bh', '<Cmd>BufferLineMovePrev<CR>', desc = 'Move Tab Prev' },

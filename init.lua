e3827e3a6b904d4c3ef1438dbf2d02a552c1f781
Luis Vinicius <luisvinicius0906@gmail.com>
2025-08-06 09:52:14 -0300

    add vim-tmux-navigator

--- init.lua
+++ init.lua
@@ -321,6 +321,8 @@ require('lazy').setup({
 
   require 'kickstart.plugins.comment',
 
+  require 'kickstart.plugins.vim-tmux-navigation',
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --

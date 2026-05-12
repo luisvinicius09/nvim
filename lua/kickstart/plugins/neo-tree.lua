39a1490f5cb9c9283bde98314cf20538cec36267
Luis Vinicius <luisvinicius0906@gmail.com>
2026-05-12 15:24:49 -0300

    feat(neo-tree): remember window width on resize

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -13,17 +13,39 @@ return {
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
-  opts = {
-    close_if_last_window = true,
-    event_handlers = {
-      {
-        event = 'file_opened',
-        handler = function()
-          require('neo-tree.command').execute { action = 'close' }
+  opts = function()
+    local neo_tree_width = 40
+
+    vim.api.nvim_create_autocmd('WinResized', {
+      group = vim.api.nvim_create_augroup('neo-tree-remember-width', { clear = true }),
+      callback = function()
+        for _, win in ipairs(vim.v.event.windows or {}) do
+          if vim.api.nvim_win_is_valid(win) then
+            local buf = vim.api.nvim_win_get_buf(win)
+            if vim.bo[buf].filetype == 'neo-tree' then
+              neo_tree_width = vim.api.nvim_win_get_width(win)
+            end
+          end
+        end
+      end,
+    })
+
+    return {
+      close_if_last_window = true,
+      event_handlers = {
+        {
+          event = 'file_opened',
+          handler = function()
+            require('neo-tree.command').execute { action = 'close' }
+          end,
+        },
+      },
+      window = {
+        width = function()
+          return neo_tree_width
         end,
       },
-    },
-    filesystem = {
+      filesystem = {
       window = {
         position = 'right',
         mappings = {
@@ -35,5 +57,6 @@ return {
         hide_dotfiles = false,
       },
     },
-  },
+    }
+  end,
 }
